e122b5da7d1277921747095583756aed1b7448f8
Rabea Gleissner <rabeagleissner@gmail.com>
2016-09-02 21:07:11 +0100

    displays posts for an apprentice

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,8 @@ defmodule Feedbacker.PageController do
   use Feedbacker.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    %{body: body} = HTTPoison.get!("https://mollies.github.io/feed.xml")
+    posts = Posts.for(body)
+    render conn, "index.html", posts: posts
   end
 end
